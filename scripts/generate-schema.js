0c8ca773a396b0c4c4a3aeec842a9bd57bdace51
Madusha Prasanjith <hi@madusha.me>
2022-09-22 00:15:17 +0530

    feat: Update gitignore and cleanup repo.

--- scripts/generate-schema.js
+++ scripts/generate-schema.js
@@ -1,27 +1,28 @@
-const fs = require('fs')
-const path = require('path')
-const glob = require('glob')
+const fs = require('fs');
+const glob = require('glob');
 
 /**
  * Merge all schemas (files with `.graphql` extension) from `mappings` into a single schema
  */
-async function generateSchema () {
+async function generateSchema() {
   const files = await new Promise((resolve, reject) =>
-    glob(`${__dirname}/../src/mappings/**/schema.graphql`, (err, files) => (err ? reject(err) : resolve(files)))
-  )
+    glob(`${__dirname}/../src/mappings/**/schema.graphql`, (err, files) =>
+      err ? reject(err) : resolve(files)
+    )
+  );
   const schema = [...files]
-    .map(file => {
-      const name = path.basename(path.dirname(file))
-      const content = fs.readFileSync(file, 'utf-8')
-      return `# START ${name}\n${content}\n# END ${name}`
-    })
-    .join('\n\n')
+    .map(file => fs.readFileSync(file, 'utf-8'))
+    .join('\n\n');
 
-  fs.writeFileSync(`${__dirname}/../schema.graphql`, schema, 'utf-8')
+  fs.writeFileSync(`${__dirname}/../schema.graphql`, schema, 'utf-8');
 }
 
-if ((require.main === module)) {
-  generateSchema().catch((err) => { console.log(err); process.exit(1) })
+if (require.main === module) {
+  generateSchema().catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
 } else {
-  module.exports = generateSchema
+  module.exports = generateSchema;
 }
+
