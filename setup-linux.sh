b2a173dd7f5b9625a3b19b8d1121b2dbc3dec6f5
Dino <crescimbenidino@gmail.com>
2022-11-21 16:26:51 -0300

    fixed setup for linux

--- setup-linux.sh
+++ setup-linux.sh
@@ -7,27 +7,22 @@ if ! which docker 2>&1 > /dev/null; then
     exit 1
 fi
 
-if ! which docker-compose 2>&1 > /dev/null; then
-    echo "Please install 'docker-compose' first"
-    exit 1
-fi
-
 if ! which jq 2>&1 > /dev/null; then
     echo "Please install 'jq' first"
     exit 1
 fi
 
 # Create the graph-node container
-docker-compose up --no-start graph-node
+sudo docker compose up --no-start graph-node
 
 # Start graph-node so we can inspect it
-docker-compose start graph-node
+sudo docker compose start graph-node
 
 # Identify the container ID
-CONTAINER_ID=$(docker container ls | grep graph-node | cut -d' ' -f1)
+CONTAINER_ID=$(sudo docker container ls | grep graph-node | cut -d' ' -f1)
 
 # Inspect the container to identify the host IP address
-HOST_IP=$(docker inspect "$CONTAINER_ID" | jq -r .[0].NetworkSettings.Networks[].Gateway)
+HOST_IP=$(sudo docker inspect "$CONTAINER_ID" | jq -r .[0].NetworkSettings.Networks[].Gateway)
 
 echo "Host IP: $HOST_IP"
 
@@ -36,7 +31,7 @@ sed -i -e "s/host.docker.internal/$HOST_IP/g" docker-compose.yml
 
 function stop_graph_node {
     # Ensure graph-node is stopped
-    docker-compose stop graph-node
+    docker compose stop graph-node
 }
 
 trap stop_graph_node EXIT
