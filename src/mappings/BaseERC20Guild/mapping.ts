15a615a2b264d47a2dae2d048e427834e5685916
Tomas Pulenta <tpulenta@gmail.com>
2022-11-22 14:44:27 -0300

    adding proposals

--- src/mappings/BaseERC20Guild/mapping.ts
+++ src/mappings/BaseERC20Guild/mapping.ts
@@ -1,5 +1,7 @@
-import { Guild } from '../../types/schema';
+import { Bytes, Value } from '@graphprotocol/graph-ts';
+import { Guild, Proposal } from '../../types/schema';
 import {
+  BaseERC20Guild,
   ProposalStateChanged,
   SetConfigCall,
   TokensLocked,
@@ -34,7 +36,31 @@ export function handleConfigChange(call: SetConfigCall): void {
   guild.save();
 }
 
-export function handleProposalStateChange(event: ProposalStateChanged): void {}
+export function handleProposalStateChange(event: ProposalStateChanged): void {
+  let address = event.address;
+  let contract = BaseERC20Guild.bind(address);
+
+  let proposal = Proposal.load(event.params.proposalId.toHexString());
+
+  const proposalData = contract.getProposal(event.params.proposalId);
+
+  if (proposal == null) {
+    proposal = new Proposal(event.params.proposalId.toHexString());
+    proposal.guildId = address.toHexString();
+    proposal.creator = proposalData.creator.toHexString();
+    proposal.startTime = proposalData.startTime;
+    proposal.endTime = proposalData.endTime;
+    proposal.to = proposalData.to.map<string>(d => d.toHexString());
+    proposal.data = proposalData.data.map<string>(d => d.toHexString());
+    proposal.value = proposalData.value;
+    proposal.title = proposalData.title;
+    proposal.contentHash = proposalData.contentHash;
+    proposal.totalVotes = proposalData.totalVotes;
+  }
+
+  proposal.contractState = event.params.newState;
+  proposal.save();
+}
 
 export function handleTokenLocking(event: TokensLocked): void {}
 
