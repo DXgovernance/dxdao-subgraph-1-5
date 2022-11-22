70775fc3daec4185668edc0a7e543779dc477544
Madusha Prasanjith <hi@madusha.me>
2022-11-22 20:40:42 +0100

    feat: Add basic configs for PermissionRegistry.

---
+++ src/mappings/PermissionRegistry/mapping.ts
@@ -0,0 +1,4 @@
+import { PermissionSet } from '../../types/PermissionRegistry/PermissionRegistry';
+import { GuildPermission } from '../../types/schema';
+
+function handleSetPermission(event: PermissionSet): void {}
