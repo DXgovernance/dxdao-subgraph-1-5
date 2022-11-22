b9b751c56303fca5d0eb39a319f56bb49e5fa36c
Madusha Prasanjith <hi@madusha.me>
2022-11-22 01:03:47 +0100

    feat: Add call handler for config changes.

--- src/mappings/BaseERC20Guild/mapping.ts
+++ src/mappings/BaseERC20Guild/mapping.ts
@@ -1,9 +1,38 @@
+import { Guild } from '../../types/schema';
 import {
   ProposalStateChanged,
+  SetConfigCall,
   TokensLocked,
   TokensWithdrawn,
   VoteAdded,
 } from '../../types/templates/BaseERC20Guild/BaseERC20Guild';
+
+export function handleConfigChange(call: SetConfigCall): void {
+  // Load Guild using the called address
+  const address = call.to;
+  let guild = Guild.load(address.toHexString());
+
+  // We shouldn't land here
+  if (guild == null) return;
+
+  // Update Guild config
+  guild.proposalTime = call.inputs._proposalTime;
+  guild.lockTime = call.inputs._lockTime;
+  guild.timeForExecution = call.inputs._timeForExecution;
+  guild.votingPowerForProposalCreation =
+    call.inputs._votingPowerForProposalCreation;
+  guild.votingPowerForProposalExecution =
+    call.inputs._votingPowerForProposalExecution;
+  guild.voteGas = call.inputs._voteGas;
+  guild.maxGasPrice = call.inputs._maxGasPrice;
+  guild.maxActiveProposals = call.inputs._maxActiveProposals;
+  guild.minimumMembersForProposalCreation =
+    call.inputs._minimumMembersForProposalCreation;
+  guild.minimumTokensLockedForProposalCreation =
+    call.inputs._minimumTokensLockedForProposalCreation;
+
+  guild.save();
+}
 
 export function handleProposalStateChange(event: ProposalStateChanged): void {}
 
@@ -12,3 +41,4 @@ export function handleTokenLocking(event: TokensLocked): void {}
 export function handleTokenWithdrawal(event: TokensWithdrawn): void {}
 
 export function handleVoting(event: VoteAdded): void {}
+
