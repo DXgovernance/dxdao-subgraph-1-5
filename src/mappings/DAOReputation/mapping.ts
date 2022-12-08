7e212314e8d658c4c6d3a55cdbae43cbca3a90ce
Dino <crescimbenidino@gmail.com>
2022-12-08 10:31:26 -0300

    added members

---
+++ src/mappings/DAOReputation/mapping.ts
@@ -0,0 +1,44 @@
+import {
+  Burn,
+  DAOReputation,
+  Mint,
+} from '../../types/templates/DAOReputation/DAOReputation';
+import { Member } from '../../types/schema';
+import { BigInt } from '@graphprotocol/graph-ts';
+
+export function handleMint(event: Mint): void {
+  const tokenAddress = event.address;
+  const repContract = DAOReputation.bind(tokenAddress);
+
+  const memberId = `${tokenAddress}-${event.params._to}`;
+  let member = Member.load(memberId);
+
+  if (!member) {
+    member = new Member(memberId);
+    member.address = event.params._to.toHexString();
+  }
+  const reputation = repContract.balanceOf(event.params._to);
+  member.reputation = reputation;
+
+  member.save();
+}
+
+export function handleBurn(event: Burn): void {
+  const tokenAddress = event.address;
+  const repContract = DAOReputation.bind(tokenAddress);
+
+  const memberId = `${tokenAddress}-${event.params._from}`;
+  let member = Member.load(memberId);
+
+  if (!member) return;
+
+  const reputation = repContract.balanceOf(event.params._from);
+  member.reputation = reputation;
+
+  if (reputation == new BigInt(0)) {
+    member.unset(memberId);
+  } else {
+    member.save();
+  }
+}
+
