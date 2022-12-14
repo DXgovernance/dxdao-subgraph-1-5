e95b2946d98ad6504e841491d82434cf6149a090
Dino <crescimbenidino@gmail.com>
2022-12-14 14:47:31 -0300

    feat: indexing votes

--- src/mappings/DXDVotingMachine/mapping.ts
+++ src/mappings/DXDVotingMachine/mapping.ts
@@ -1,43 +1,18 @@
-import { log } from '@graphprotocol/graph-ts';
-import { Proposal } from '../../types/schema';
-import { DXDVotingMachine as DXDVotingMachineContract } from '../../types/templates/DXDVotingMachine/DXDVotingMachine';
-import { NewProposal } from '../../types/templates/DXDVotingMachine/DXDVotingMachine';
+import { VoteProposal } from '../../types/templates/DXDVotingMachine/DXDVotingMachine';
+import { Vote } from '../../types/schema';
 
-export function handleNewProposal(event: NewProposal): void {
-  const votingMachineAddress = event.address;
-  const proposalId = event.params._proposalId;
-  const votingMachineContract =
-    DXDVotingMachineContract.bind(votingMachineAddress);
+export function handleVoteProposal(event: VoteProposal): void {
+  let voteId = `${event.params._proposalId.toHexString()}-${event.params._voter.toHexString()}`;
 
-  const proposalData = votingMachineContract.proposals(proposalId);
-  const proposalTimes = votingMachineContract.getProposalTimes(proposalId);
+  let vote = Vote.load(voteId);
+  if (!vote) {
+    vote = new Vote(voteId);
+  }
 
-  const proposal = new Proposal(proposalId.toHexString());
-
-  // ! Scheme properties. Hardcoded
-  proposal.to = [];
-  proposal.callData = [];
-  proposal.value = [];
-
-  proposal.schemeId = proposalData.getSchemeId().toHexString();
-  proposal.callbacks = proposalData.getCallbacks().toHexString();
-  proposal.state = 'None'; //! hardcoded
-  proposal.executionState = 'None'; //! hardcoded
-  proposal.winningVote = proposalData.getWinningVote();
-  proposal.proposer = proposalData.getProposer().toHexString();
-  proposal.currentBoostedVotePeriodLimit =
-    proposalData.getCurrentBoostedVotePeriodLimit();
-  proposal.paramsHash = proposalData.getParamsHash().toHexString();
-  proposal.daoBountyRemain = proposalData.getDaoBountyRemain();
-  proposal.daoBounty = proposalData.getDaoBounty();
-  proposal.totalStakes = proposalData.getTotalStakes();
-  proposal.confidenceThreshold = proposalData.getConfidenceThreshold();
-  proposal.secondsFromTimeOutTillExecuteBoosted =
-    proposalData.getSecondsFromTimeOutTillExecuteBoosted();
-  proposal.boostedPhaseTime = proposalTimes[1];
-  proposal.preBoostedPhaseTime = proposalTimes[2];
-  proposal.daoRedeemItsWinnings = proposalData.getDaoRedeemItsWinnings();
-
-  proposal.save();
+  vote.proposal = event.params._proposalId.toHexString();
+  vote.member = event.params._voter.toHexString();
+  vote.vote = event.params._vote;
+  vote.reputation = event.params._reputation;
+  vote.save();
 }
 
