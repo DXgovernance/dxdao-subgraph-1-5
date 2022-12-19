9c993455391172c266b345a53892917170efb462
Dino <crescimbenidino@gmail.com>
2022-12-19 15:12:37 -0300

    refactor(VotingMachine): changed how stakes and redeems work
    now every stake and redeem is tracked as a new entity, with no connection between them. Also added txId and timestamp

--- src/mappings/DXDVotingMachine/mapping.ts
+++ src/mappings/DXDVotingMachine/mapping.ts
@@ -1,5 +1,5 @@
 import { BigInt } from '@graphprotocol/graph-ts';
-import { Stake, Vote, DAO } from '../../types/schema';
+import { Stake, Vote, DAO, Redeem } from '../../types/schema';
 import {
   VoteProposal,
   Stake as StakeEvent,
@@ -28,8 +28,12 @@ export function handleVoteProposal(event: VoteProposal): void {
 }
 
 export function handleStake(event: StakeEvent): void {
-  const stakeId = `${event.params._proposalId.toHexString()}-${event.params._staker.toHexString()}`;
+  const stakeId = `${event.params._proposalId.toHexString()}-${event.params._staker.toHexString()}-${
+    event.block.timestamp
+  }-stake`;
 
+  // In the most common case, each staking is unique
+  // We load to handle the edge case of someone staking twice on the same block
   let stake = Stake.load(stakeId);
   if (!stake) {
     stake = new Stake(stakeId);
@@ -40,18 +44,32 @@ export function handleStake(event: StakeEvent): void {
   stake.avatar = event.params._avatar.toHexString();
   stake.staker = event.params._staker.toHexString();
   stake.vote = event.params._vote;
-  stake.amount = stake.amount.plus(event.params._amount);
+  stake.amount = event.params._amount;
+  stake.timestamp = event.block.timestamp;
+  stake.txId = event.transaction.hash.toHexString();
 
   stake.save();
 }
 
 export function handleRedeem(event: RedeemEvent): void {
-  const stakeId = `${
+  const redeemId = `${
     event.params._proposalId
-  }-${event.params._beneficiary.toHexString()}`;
-  const stake = Stake.load(stakeId);
-  if (!stake) return;
+  }-${event.params._beneficiary.toHexString()}-${event.block.timestamp}-redeem`;
 
-  stake.amount = stake.amount.minus(event.params._amount);
-  stake.save();
+  // In the most common case, each redeem is unique
+  // We load to handle the edge case of someone redeeming twice on the same block
+  let redeem = Redeem.load(redeemId);
+  if (!redeem) {
+    redeem = new Redeem(redeemId);
+  }
+
+  redeem.proposal = event.params._proposalId.toHexString();
+  redeem.avatar = event.params._avatar.toHexString();
+  redeem.redeemer = event.params._beneficiary.toHexString();
+  redeem.amount = event.params._amount;
+  redeem.timestamp = event.block.timestamp;
+  redeem.txId = event.transaction.hash.toHexString();
+
+  redeem.save();
 }
+
